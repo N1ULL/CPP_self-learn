015b57f87b2baa448050f08e59406292d230f6d6
N1ULL <34934155+N1ULL@users.noreply.github.com>
2022-08-04 14:11:10 +0800

    Add files via upload

---
+++ 转义字符.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //转义字符
+
+    //换行符 \n
+
+    cout << "hello world\n";
+
+    //反斜杠 \\
+
+    cout << "\\" << endl;
+    
+    //水平制表符 \t
+    cout << "hello world\t";
+    
+}
